e3c0338892cda1ca76e271b4ab0ec2f8fb22c830
XuanThongN <thong.xuan.vft@gmail.com>
2024-03-05 23:04:56 +0700

    feat: add login with MVP architect

--- app/src/main/java/com/xuanthongn/MainActivity.java
+++ app/src/main/java/com/xuanthongn/MainActivity.java
@@ -1,16 +1,38 @@
 package com.xuanthongn;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-import com.xuanthongn.R;
+import com.xuanthongn.base.BaseActivity;
+import com.xuanthongn.data.model.Product;
+import com.xuanthongn.ui.constract.IMainConstract;
+import com.xuanthongn.ui.presenter.MainPresenter;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends BaseActivity implements IMainConstract.IView {
+    private RecyclerView mRvHotProduct;
+    private IMainConstract.IPresenter mPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPresenter = new MainPresenter(this);
+        mPresenter.setView(this);
+
+        initGUI();
+    }
+
+    private void initGUI() {
+        mRvHotProduct = findViewById(R.id.rv_hot_product);
+    }
+
+    @Override
+    public void setHotProductsToView(List<Product> productList) {
+
     }
 }
---
+++ app/src/main/java/com/xuanthongn/ui/adapter/ProductAdapter.java
@@ -0,0 +1,4 @@
+package com.xuanthongn.ui.adapter;
+
+public class ProductAdapter {
+}
