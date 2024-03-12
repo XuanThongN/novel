aa446e76ad558ae32d2dd2b8889a74ec53c34b42
XuanThongN <thong.xuan.vft@gmail.com>
2024-03-12 21:17:55 +0700

    feat: add carousel ui

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         release {
             isMinifyEnabled = false
             proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
             )
         }
     }
@@ -40,22 +40,28 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation ("androidx.room:room-runtime:2.6.1")
-    annotationProcessor ("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-runtime:2.6.1")
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
 
     // optional - RxJava2 support for Room
-    implementation ("androidx.room:room-rxjava2:2.6.1")
+    implementation("androidx.room:room-rxjava2:2.6.1")
 
     // optional - RxJava3 support for Room
-    implementation ("androidx.room:room-rxjava3:2.6.1")
+    implementation("androidx.room:room-rxjava3:2.6.1")
 
     // optional - Guava support for Room, including Optional and ListenableFuture
-    implementation ("androidx.room:room-guava:2.6.1")
+    implementation("androidx.room:room-guava:2.6.1")
 
     // optional - Test helpers
-    testImplementation ("androidx.room:room-testing:2.6.1")
+    testImplementation("androidx.room:room-testing:2.6.1")
 
     // optional - Paging 3 Integration
-    implementation ("androidx.room:room-paging:2.6.1")
+    implementation("androidx.room:room-paging:2.6.1")
+
+
+    // For slider implementation
+    // Circle Indicator (To fix the xml preview "Missing classes" error)
+    implementation("me.relex:circleindicator:2.1.6")
+    implementation("org.imaginativeworld.whynotimagecarousel:whynotimagecarousel:2.1.0")
 
 }
--- app/src/main/java/com/xuanthongn/MainActivity.java
+++ app/src/main/java/com/xuanthongn/MainActivity.java
@@ -10,11 +10,18 @@ import com.xuanthongn.data.model.Product;
 import com.xuanthongn.ui.constract.IMainConstract;
 import com.xuanthongn.ui.presenter.MainPresenter;
 
+import org.imaginativeworld.whynotimagecarousel.ImageCarousel;
+import org.imaginativeworld.whynotimagecarousel.model.CarouselItem;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends BaseActivity implements IMainConstract.IView {
     private RecyclerView mRvHotProduct;
     private IMainConstract.IPresenter mPresenter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +32,45 @@ public class MainActivity extends BaseActivity implements IMainConstract.IView {
         mPresenter.setView(this);
 
         initGUI();
+
+
+        // Java
+        ImageCarousel carousel = findViewById(R.id.carousel);
+
+        // Register lifecycle. For activity this will be lifecycle/getLifecycle() and for fragments it will be viewLifecycleOwner/getViewLifecycleOwner().
+        carousel.registerLifecycle(getLifecycle());
+
+        List<CarouselItem> list = new ArrayList<>();
+
+        // Image URL with caption
+        list.add(new CarouselItem("https://images.unsplash.com/photo-1532581291347-9c39cf10a73c?w=1080","Cre: Thong"));
+
+// Just image URL
+        list.add(new CarouselItem("https://images.unsplash.com/photo-1534447677768-be436bb09401?w=1080"));
+
+// Image URL with header
+        Map<String, String> headers = new HashMap<>();
+        headers.put("header_key", "header_value");
+
+        list.add(new CarouselItem("https://images.unsplash.com/photo-1534447677768-be436bb09401?w=1080", headers)
+        );
+
+// Just image drawable
+        list.add(new CarouselItem(R.drawable.test));
+
+
+        carousel.setData(list);
     }
 
     private void initGUI() {
-        mRvHotProduct = findViewById(R.id.rv_hot_product);
+
+//        mRvHotProduct = findViewById(R.id.rv_hot_product);
     }
 
     @Override
     public void setHotProductsToView(List<Product> productList) {
 
     }
+
+
 }
