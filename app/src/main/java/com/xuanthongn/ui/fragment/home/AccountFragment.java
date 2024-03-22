f87a1ba329d32028886a3b8704a267bb71f7903a
XuanThongN <thong.xuan.vft@gmail.com>
2024-03-22 16:33:22 +0700

    feat: move fragment directory

--- app/src/main/java/com/xuanthongn/ui/fragment/home/AccountFragment.java
+++ app/src/main/java/com/xuanthongn/ui/fragment/home/AccountFragment.java
@@ -1,4 +1,4 @@
-package com.xuanthongn.ui.fragment.novel_details_fragments.home;
+package com.xuanthongn.ui.fragment.home;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,11 +9,11 @@ import androidx.fragment.app.Fragment;
 
 import com.xuanthongn.R;
 
-public class BookmarkFragment extends Fragment {
+public class AccountFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bookmark, container, false);
+        return inflater.inflate(R.layout.fragment_account, container, false);
     }
 }
