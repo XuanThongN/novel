3236c3726a5df96f19c0a2d78d936d39821abc0f
Dung Paulus CTS <linhtam2001a@gmail.com>
2024-03-23 18:06:33 +0700

    feat:customize-ui-novel-details-yourlike

--- app/src/main/java/com/xuanthongn/ui/fragment/novel_details_fragments/InformationFragment.java
+++ app/src/main/java/com/xuanthongn/ui/fragment/novel_details_fragments/InformationFragment.java
@@ -16,10 +16,12 @@ import com.xuanthongn.data.model.CategoryItem;
 import com.xuanthongn.data.model.Novel;
 import com.xuanthongn.data.model.NovelComment;
 import com.xuanthongn.data.model.NovelRecommend;
+import com.xuanthongn.data.model.NovelYourLikes;
 import com.xuanthongn.ui.adapter.CategoryItemAdapter;
 import com.xuanthongn.ui.adapter.CategoryNovelItemAdapter;
 import com.xuanthongn.ui.adapter.NovelContinueReadingAdapter;
 import com.xuanthongn.ui.adapter.NovelDetailsCommentAdapter;
+import com.xuanthongn.ui.adapter.NovelDetailsYourlikeAdapter;
 import com.xuanthongn.ui.adapter.NovelRecommendAdapter;
 
 import org.imaginativeworld.whynotimagecarousel.ImageCarousel;
@@ -46,7 +48,7 @@ public class InformationFragment extends Fragment {
     private void initGUI(View view) {
         Context context = this.getContext();
 
-//        View continue reading
+//        Truyền dữ liệu vào thể loại truyện
         RecyclerView rvContinueReading = view.findViewById(R.id.rv_continue_category_novel);
         List<Category> category = new ArrayList<>();
         category.add(new Category(1, "Truyền Thuyết"));
@@ -57,12 +59,27 @@ public class InformationFragment extends Fragment {
 
         rvContinueReading.setAdapter(new CategoryNovelItemAdapter(context, category));
 
-        //        View continue reading
+//        Truyền dữ liệu vào  bình luận
         RecyclerView rvContinueComment= view.findViewById(R.id.rv_continue_comment_novel);
         List<NovelComment> novelComment = new ArrayList<>();
         novelComment.add(new NovelComment(1, "https://images.unsplash.com/photo-1532581291347-9c39cf10a73c?w=1080", "dung1@gmail.com", "helo", 145, new Date(102, 1, 12), 3, 4));
+        novelComment.add(new NovelComment(2, "https://images.unsplash.com/photo-1532581291347-9c39cf10a73c?w=1080", "dung1@gmail.com", "helo", 145, new Date(102, 1, 12), 3, 4));
+        novelComment.add(new NovelComment(3, "https://images.unsplash.com/photo-1532581291347-9c39cf10a73c?w=1080", "dung1@gmail.com", "helo", 145, new Date(102, 1, 12), 3, 4));
+        novelComment.add(new NovelComment(4, "https://images.unsplash.com/photo-1532581291347-9c39cf10a73c?w=1080", "dung1@gmail.com", "helo", 145, new Date(102, 1, 12), 3, 4));
 
         rvContinueComment.setAdapter(new NovelDetailsCommentAdapter(context, novelComment));
+
+
+        //        Truyền dữ liệu vào mục truyện bạn có thể thích
+        RecyclerView rvContinueYourlike= view.findViewById(R.id.rv_continue_yourlike_novel);
+        List<NovelYourLikes> novelYourlike = new ArrayList<>();
+        novelYourlike.add(new NovelYourLikes(1, "https://images.unsplash.com/photo-1532581291347-9c39cf10a73c?w=1080", "Hoài Ngọc truyền kỳ", "Ngày xửa ngày xưa ở một ngôi nhà nọ"));
+        novelYourlike.add(new NovelYourLikes(2, "https://images.unsplash.com/photo-1532581291347-9c39cf10a73c?w=1080", "Hoài Ngọc truyền kỳ", "Ngày xửa ngày xưa ở một ngôi nhà nọ"));
+        novelYourlike.add(new NovelYourLikes(3, "https://images.unsplash.com/photo-1532581291347-9c39cf10a73c?w=1080", "Hoài Ngọc truyền kỳ", "Ngày xửa ngày xưa ở một ngôi nhà nọ"));
+        novelYourlike.add(new NovelYourLikes(3, "https://images.unsplash.com/photo-1532581291347-9c39cf10a73c?w=1080", "Hoài Ngọc truyền kỳ", "Ngày xửa ngày xưa ở một ngôi nhà nọ"));
+
+        rvContinueYourlike.setAdapter(new NovelDetailsYourlikeAdapter(context, novelYourlike));
+
     }
 
 }
